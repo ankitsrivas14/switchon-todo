388b53c9c837595f0b562081b6c3b0a57583607b
Ankit Srivastava <ankitsrivas14@gmail.com>
2020-11-20 04:36:24 +0530

    Logout Implemented

--- src/Components/Logout/Logout.js
+++ src/Components/Logout/Logout.js
@@ -1,9 +1,13 @@
 import React from "react"
-import { Redirect, useHistory } from "react-router-dom";
+import {useHistory } from "react-router-dom";
 
 export const Logout = () => {
     const history = useHistory()
     localStorage.removeItem("token");
     localStorage.removeItem("user");
-    history.push("/login")
+    history.push("/login");
+    return(
+        null
+    )
+    
 }
